c3e9fd5bdd46577f89feaa3e1fb0c5b23d6642a5
devangpatel <devang.patel@collabsoftech.com.au>
2025-08-14 10:08:58 +0530

    auth api chnages

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,24 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
+  base: "./",
   server: {
     host: "0.0.0.0",
+    proxy: {
+      '/api': {
+        target: process.env.VITE_API_URL,
+        changeOrigin: true,
+        secure: true,
+      },
+    },
   },
+});
 
   //  build: {
   //   rollupOptions: {
   //     external: ['@mui/styled-engine'],
   //   },
   // },
-});
